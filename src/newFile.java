1ee4d604a15bb658178e26297eed1bea1b3c44f6
OlehPetryshyn <olegpetryshen@gmail.com>
2023-04-02 12:51:26 +0300

    SecondCommit

---
+++ src/newFile.java
@@ -0,0 +1,13 @@
+public class newFile {
+
+        public int numberValue = 17;
+
+        public String print(){
+            String addValue = String.format("\"---<%d>---\"",numberValue);
+            System.out.println(addValue);
+            return addValue;
+        }
+
+
+
+    }
